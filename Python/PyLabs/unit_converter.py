d4663f79a24b054794a17e08d199f43a51225df9
Timothy <tim.ttaj@gmail.com>
2022-12-28 19:44:06 -0800

    pylabs unit converter

---
+++ Python/PyLabs/unit_converter.py
@@ -0,0 +1,8 @@
+# LAB 01: UNIT CONVERTER
+# This lab will involve writing a program that allows the user to convert a
+# number between units.
+
+# VERSION 1
+# Ask the user for the number of feet, and print out the equivalent distance in
+# meters. Hint: 1 ft is 0.3048 m. So we can get the output in meters by
+# multiplying the input distance by 0.3048.
