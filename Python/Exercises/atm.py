b59c3b4836199f2a765f641c369e7d7614601a9a
ttajohnson <tim.ttaj@gmail.com>
2023-06-25 10:41:24 -0700

    ATM start

---
+++ Python/Exercises/atm.py
@@ -0,0 +1,28 @@
+"""
+Represent an ATM with a class containing two attributes: a balance and an interest rate.
+A newly created account will default to a balance of 0 and an interest rate of 0.1%.
+"""
+
+class ATM():
+    
+    def __init__(self):
+        self.balance = 0
+        self.interest_rate = 0.001
+
+    def check_balance(self):
+        return
+    
+    def deposit(self, amount):
+        return
+    
+    def check_withdrawal(self, amount):
+        return
+    
+    def withdraw(self, amount):
+        return
+    
+    def calc_interest(self):
+        return
+    
+    
+    
