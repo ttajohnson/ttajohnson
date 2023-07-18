f8be0d291b7defa4747b9536efbf72e02a65486a
ttajohnson <tim.ttaj@gmail.com>
2023-07-18 16:57:47 -0700

    Tic Tac Toe Python Start

---
+++ Python/Exercises/tictactoe.py
@@ -0,0 +1,25 @@
+class Player:
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
+
+class Game(Player):
+    def __init__(self):
+        self.board = [
+            
+        ]
+
+    def __repr__():
+        pass
+
+    def move(x,y, player):
+        pass
+
+    def calc_winner():
+        pass
+
+    def is_full():
+        pass
+
+    def is_game_over():
+        pass
