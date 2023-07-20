081a2cb72f2a376c22be551c5e96709b2dcd72d8
ttajohnson <tim.ttaj@gmail.com>
2023-07-19 17:15:23 -0700

    Tic Tac Toe, Continuation

--- Python/Exercises/tictactoe.py
+++ Python/Exercises/tictactoe.py
@@ -5,21 +5,43 @@ class Player:
 
 class Game(Player):
     def __init__(self):
-        self.board = [
-            
-        ]
+        # self.board = [
+        #     ['-','-','-'], # 0,0 | 0,1 | 0,2
+        #     ['-','-','-'], # 1,0 | 1,1 | 1,2
+        #     ['-','-','-']  # 2,0 | 2,1 | 2,2
 
-    def __repr__():
+        #     # OR
+
+        # ]
+        self.board = [['-' for _ in range(3)] for _ in range(3)]
+
+    def __repr__(self):
+        # board_str = ''
+        # for row in self.board:
+        #     board_str += '|'.join(row) + '\n'
+        # return board_str
+
+        # # OR
+
+        return '\n'.join(['|'.join(row) for row in self.board])
+
+
+
+    def move(self, x, y, player):
+        self.board[x][y] = Player.token
+
+    def calc_winner(self):
         pass
 
-    def move(x,y, player):
+    def is_full(self):
+        if '-' in self.board:
+            print("It's a draw!")
+
+    def is_game_over(self):
         pass
 
-    def calc_winner():
-        pass
 
-    def is_full():
-        pass
 
-    def is_game_over():
-        pass
+ttt = Game()
+
+print(ttt)
